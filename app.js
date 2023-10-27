22516053866059e389b28d44ca0887417f642c75
Adegbuyi Daniel <adegbuyidaniel42@gmail.com>
2023-10-27 18:25:15 +0100

    Update

--- app.js
+++ app.js
@@ -13,6 +13,73 @@ const multiplicationEl =  document.querySelector('.multiplication');
 const divisionEl =  document.querySelector('.division');
 const equalEl =  document.querySelector('.equal');
 
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+    number0El, number1El, number2El, number3El, number4El,
+    number5El, number6El, number7El, number8El, number9El
+];
+
+// Functions
+const getDisplayAsStr = () => {
+    const currentDisplayStr = displayEl.textContent;
+    return currentDisplayStr.split(',').join('');
+}
+
+const getDisplayAsNum = () => {
+    return parseFloat(getDisplayAsStr());
+}
+
+const setStrAsDisplay = (displayStr) => {
+    if (displayStr[displayStr.length - 1] === '.') {
+        displayEl.textContent += '.';
+        return; 
+    }
+
+    const [wholeNumStr, decimalStr] = displayStr.split('.');
+    if (decimalStr) {
+        displayEl.textContent = parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+    } else {
+       displayEl.textContent = parseFloat(wholeNumStr).toLocaleString(); 
+    }   
+}
+
+const handleNumberClick = (numStr) => {
+    const currentDisplayStr = getDisplayAsStr();
+    if (currentDisplayStr === '0') {
+        setStrAsDisplay(numStr);
+    } else {
+        setStrAsDisplay(currentDisplayStr + numStr);
+    }
+}
+
+// Add Event Listeners to functions
+
+
+// Add Event Listeners to numbers and buttons
+for (let i=0; i < numberElArray.length; i++) {
+    const numberEl = numberElArray[i];
+    numberEl.addEventListener('click', () => {
+        handleNumberClick(i.toString());
+    })
+}
+decimalEl.addEventListener('click', () => {
+    const currentDisplayStr = getDisplayAsStr();
+    if (!currentDisplayStr.includes('.')) {
+        setStrAsDisplay(currentDisplayStr + '.');
+        displayEl.textContent = currentDisplayStr + '.';
+    }
+})
+
 // Update time
 const updateTime = () => {
     const currentTime = new Date();
